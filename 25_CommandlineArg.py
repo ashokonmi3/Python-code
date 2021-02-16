7b2174292e2ca6d79967ca1fa060c662fbb6c95c
ashokonmi3 <ashokonmi3@gmail.com>
2021-02-16 09:08:36 +0530

    Add files via upload

--- 25_CommandlineArg.py
+++ 25_CommandlineArg.py
@@ -1,32 +1,39 @@
-import sys
+# import sys
+# print(" This is commmand line python program execution")
 # program_name = sys.argv[0]
-print ("sys.argv is :", sys.argv)
+# print ("sys.argv is :", sys.argv)
 # print ("program name is : ", program_name)
-# l=[]
-# l=sys.argv
-# print l
-# print l[0]
-# print dir(sys)
-# print (program_name)
-arguments = sys.argv[1:]
-print ("arguments are " ,arguments)
-count = len(arguments)
-print ("the number of parameters passed is :",count)
-# print sys.argv
-
-# print ("java "+sys.argv[1]+" "+ sys.argv[2])
-
+# # ==========================
+# import sys
+# arguments=[]
+# arguments=sys.argv
+# # print (l[0])
+# arguments = sys.argv[1:]
+# print ("arguments are " ,arguments)
+# count = len(arguments)
+# print ("the number of parameters passed is :",count)
+# =========================
+# Reverst of string 
+# import sys
 # s=sys.argv[1]
-# print type(s)
-# print sys.argv
-# print (s)
-# print (s[::-1])
-# s=sys.argv[1]
+# print ("Current string is : ", s)
+# print ("Reverse of string : " ,s[::-1])
+# =========================
+# Reverse of list
+# import sys
+# l=sys.argv[1:]
+# print ("Current list  is : ", l)
+# print ("Reverse of list : " ,l[::-1])
+# ======================
 
-# if s==s[::-1]:
-# 	print ("pellindrome")
-# else:
-# 	print ("not pellindrom")
+# Pellindrome of string 
+import sys
+
+s=sys.argv[1]
+if s==s[::-1]:
+	print ("pellindrome")
+else:
+	print ("not pellindrom")
 	
 # python .\25_CommandlineArg.py kayak.txt asdfadfadasdfasdfasdf asfdasdfad asdfasdfasdf asdfasdf
 # fp=open(sys.argv[1],'w')
