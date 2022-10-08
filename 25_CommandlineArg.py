5ed771c122b506a0dc886f42c13c5c4875b0efbe
ashokonmi3 <ashokonmi3@gmail.com>
2022-10-08 12:27:39 +0530

    Add files via upload

--- 25_CommandlineArg.py
+++ 25_CommandlineArg.py
@@ -1,39 +1,31 @@
+# cd C:\Users\91973\Python_Training
+# =========================
+# print("hello world")
+# print("command line argument")
+# ===========================
+# import sys
+# print(" The command line parameter is ")
+# print ( sys.argv)
+# ==========================
 # import sys
 # print(" This is commmand line python program execution")
-# program_name = sys.argv[0]
 # print ("sys.argv is :", sys.argv)
-# print ("program name is : ", program_name)
+# print ("program name is : ", sys.argv[0])
+# count = len(sys.argv)
+# print ("the number of parameters passed is len(sys.argv) :",count-1)
 # # ==========================
-# import sys
-# arguments=[]
-# arguments=sys.argv
-# # print (l[0])
-# arguments = sys.argv[1:]
-# print ("arguments are " ,arguments)
-# count = len(arguments)
-# print ("the number of parameters passed is :",count)
-# =========================
-# Reverst of string 
-# import sys
-# s=sys.argv[1]
-# print ("Current string is : ", s)
-# print ("Reverse of string : " ,s[::-1])
-# =========================
-# Reverse of list
-# import sys
-# l=sys.argv[1:]
-# print ("Current list  is : ", l)
-# print ("Reverse of list : " ,l[::-1])
-# ======================
-
-# Pellindrome of string 
+# Reverse of string 
 import sys
-
 s=sys.argv[1]
+print (f"Input string is :  {s}")
+print (f"Reverse of string : {s[::-1]}")
 if s==s[::-1]:
 	print ("pellindrome")
 else:
 	print ("not pellindrom")
+# =========================
+
+
 	
 # python .\25_CommandlineArg.py kayak.txt asdfadfadasdfasdfasdf asfdasdfad asdfasdfasdf asdfasdf
 # fp=open(sys.argv[1],'w')
